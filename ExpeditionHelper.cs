abf5bb3da591c5c176c161ff31bebd6b6e2ce569
batebono <56904621+batebono@users.noreply.github.com>
2022-03-26 23:35:00 +0900

    Add files via upload

---
+++ ExpeditionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared;
+using ExileCore.Shared.Abstract;
+using ExileCore.Shared.Cache;
+using ExileCore.Shared.Enums;
+using ExileCore.Shared.Helpers;
+using SharpDX;
+
+namespace ExpeditionHelper
+{
+    public class ExpeditionHelper : BaseSettingsPlugin<ExpeditionHelperSettings>
+    {
+    }
+}
---
+++ ExpeditionHelperSettings.cs
@@ -0,0 +1,14 @@
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace ExpeditionHelper
+{
+    public class ExpeditionHelperSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(true);
+
+        [Menu("Draw Monster")]
+        public ToggleNode DrawMonsters { get; set; } = new ToggleNode(true);
+    }
+}
